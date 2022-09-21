040bd92de487828486e59958fec18486dcb9441a
Eric Harris-Braun <eric@harris-braun.com>
2022-09-21 16:38:12 -0400

    WIP applet tsc compilation

--- apps/applet/ui/src/index.ts
+++ apps/applet/ui/src/index.ts
@@ -1,7 +1,7 @@
-import { WeApplet } from '@lightningrodlabs/we-applet';
+import type { WeApplet } from '@lightningrodlabs/we-applet';
 import { Controller } from '@holo-host/talking-stickies';
 import { HolochainClient } from '@holochain-open-dev/cell-client';
-import { AppWebsocket, DnaHash } from '@holochain/client';
+import type { AppWebsocket } from '@holochain/client';
 
 const talkingStickies: WeApplet = {
  async appletRenderers(appWebsocket: AppWebsocket, adminWs, weServices, appletInfo) {
