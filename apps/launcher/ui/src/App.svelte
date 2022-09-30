2447b93e83a6482b968d1cdcf6cb7a2a7ecee471
Eric Harris-Braun <eric@harris-braun.com>
2022-09-30 16:39:25 -0400

    update launcher app to use CellClient

--- apps/launcher/ui/src/App.svelte
+++ apps/launcher/ui/src/App.svelte
@@ -1,15 +1,13 @@
 <script lang="ts">
   import {Controller} from '@holo-host/talking-stickies'
-  import { HolochainClient } from '@holochain-open-dev/cell-client';
+  import { HolochainClient, CellClient } from '@holochain-open-dev/cell-client';
   const appId = process.env.SVELTE_APP_APP_ID ? process.env.SVELTE_APP_APP_ID : 'talking-stickies'
   const appPort = process.env.SVELTE_APP_APP_PORT ? process.env.SVELTE_APP_APP_PORT : 8888
   import { AppWebsocket, InstalledCell, type InstalledAppInfo } from '@holochain/client';
   const url = `ws://localhost:${appPort}`;
 
-  let talkingStickiesCell: InstalledCell
-  let client : HolochainClient
+  let cellClient: CellClient  
 
-  console.log("CLIENT: ", client);
   let connected = false
   initialize()
 
@@ -18,15 +16,17 @@
     const appWebsocket = await AppWebsocket.connect(url);
     
     console.log('Connecting with', appPort, appId)
-    client = new HolochainClient(appWebsocket);
+    const holochainClient = new HolochainClient(appWebsocket);
     
-    const appInfo = await client.appWebsocket.appInfo({
+    const appInfo = await holochainClient.appWebsocket.appInfo({
       installed_app_id: appId,
     });
     const installedCells = appInfo.cell_data;
-    talkingStickiesCell = installedCells.find(
+    const talkingStickiesCell = installedCells.find(
         c => c.role_id === 'talking-stickies'
       ) as InstalledCell;
+
+    cellClient = new CellClient(holochainClient, talkingStickiesCell)
 
     connected = true
   }
@@ -41,7 +41,7 @@
 <svelte:head>
 </svelte:head>
 {#if connected}
-  <Controller talkingStickiesCell={talkingStickiesCell} client={client}></Controller>
+  <Controller client={cellClient}></Controller>
 {:else}
   Loading
 {/if}
