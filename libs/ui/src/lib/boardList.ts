4293426f10e3fcb1db261e513cc3bcc2923a0b01
Eric Harris-Braun <eric@harris-braun.com>
2022-10-09 13:27:34 -0400

    missed file

---
+++ libs/ui/src/lib/boardList.ts
@@ -0,0 +1,269 @@
+import { RootStore, type Commit, type SynGrammar, type SynStore, type Workspace, type WorkspaceStore } from "@holochain-syn/core";
+import type { Dictionary, EntryHashB64 } from "@holochain-open-dev/core-types";
+import { Board, CommitTypeBoard } from "./board";
+import { deserializeHash, EntryHashMap, EntryRecord } from "@holochain-open-dev/utils";
+import { get, writable, type Readable, type Writable } from "svelte/store";
+import { boardGrammar, type BoardDelta, type BoardGrammar, type BoardState } from "./board";
+import type { AgentPubKey, EntryHash } from "@holochain/client";
+
+export const CommitTypeBoardList :string = "board-list"
+
+export interface BoardRecord {
+    hash: EntryHashB64
+    name: string
+    status: string
+}
+
+export interface BoardListState {
+    boards: BoardRecord[];
+}
+
+
+export type BoardListDelta =
+  | {
+    type: "add-board";
+    hash: EntryHashB64;
+    name: string;
+    status?: string;
+  }
+  | {
+    type: "set-name";
+    hash: EntryHashB64;
+    name: string;
+  }
+  | {
+    type: "set-status";
+    hash: EntryHashB64;
+    status: string;
+  }
+  | {
+    type: "set-index";
+    hash: EntryHashB64;
+    index: number;
+  };
+
+export type BoardListGrammar = SynGrammar<
+BoardListDelta,
+BoardListState
+>;
+
+export const boardListGrammar: BoardListGrammar = {
+    initState(state)  {
+        state.boards = []
+    },
+    applyDelta( 
+        delta: BoardListDelta,
+        state: BoardListState,
+        _ephemeralState: any,
+        _author: AgentPubKey
+      ) {
+        if (delta.type == "add-board") {
+            const record: BoardRecord = {
+                name: delta.name,
+                hash: delta.hash,
+                status: delta.status,
+            }
+            state.boards.unshift(record)
+        }
+        if (delta.type == "set-name") {
+            state.boards.forEach((board, i) => {
+                if (board.hash === delta.hash) {
+                  state.boards[i].name = delta.name;
+                }
+            });
+        }
+        if (delta.type == "set-status") {
+            state.boards.forEach((board, i) => {
+                if (board.hash === delta.hash) {
+                  state.boards[i].status = delta.status;
+                }
+            });
+        }
+        if (delta.type == "set-index") {
+            const index = state.boards.findIndex((board) => board.hash == delta.hash)
+            if (index >= 0) {
+              const c = state.boards[index]
+              state.boards.splice(index,1)
+              state.boards.splice(index, 0, c)
+            }
+          }
+        }
+    }
+
+
+export class BoardList {
+    public workspace: WorkspaceStore<BoardListGrammar>
+    public boards: Dictionary<Board>
+    activeBoardHash: Writable<EntryHashB64| undefined> = writable(undefined)
+
+    constructor(public rootStore: RootStore<BoardListGrammar>, public boardsRootStore: RootStore<BoardGrammar>) {
+        this.boards = {}
+    }
+
+    public static async Create(synStore: SynStore) {
+        const rootStore = await synStore.createDeterministicRoot(boardListGrammar, {type: CommitTypeBoardList})
+        const boardsRootStore = await synStore.createDeterministicRoot(boardGrammar, {type: CommitTypeBoard})
+        const me = new BoardList(rootStore, boardsRootStore);
+        const workspaceHash = await rootStore.createWorkspace(
+            'main',
+            rootStore.root.entryHash
+           );
+        me.workspace = await rootStore.joinWorkspace(workspaceHash)
+        return me
+    }
+    public static async Join(synStore: SynStore, rootCommit: EntryRecord<Commit>, boardsRootCommit: EntryRecord<Commit>) {
+        const rootStore = new RootStore(
+            synStore.client,
+            boardListGrammar,
+            rootCommit
+          );
+          const boardsRootStore = new RootStore(
+            synStore.client,
+            boardGrammar,
+            boardsRootCommit
+          );
+        const me = new BoardList(rootStore, boardsRootStore);
+        const workspaces: EntryHashMap<Workspace> = get(await rootStore.fetchWorkspaces());
+        // if there is no workspace then we have a problem!!
+        me.workspace = await rootStore.joinWorkspace(workspaces.keys()[0]);
+        return me
+    }
+    hash() : EntryHash {
+        return this.rootStore.root.entryHash
+    }
+    close() {
+        this.workspace.leaveWorkspace()
+    }
+    stateStore() {
+        return this.workspace.state
+    }
+    state() {
+        return get(this.workspace.state)
+    }
+    requestChanges(deltas: Array<BoardListDelta>) {
+        console.log("REQUESTING BOARDLIST CHANGES: ", deltas)
+        this.workspace.requestChanges(deltas)
+    }
+    participants()  {
+        return this.workspace.participants
+    }
+    async commitChanges() {
+        this.workspace.commitChanges()
+    }
+
+    async requestBoardChanges(hash: EntryHashB64, deltas: BoardDelta[]) {
+        const board = await this.getBoard(hash)
+        if (board) {
+            board.requestChanges(deltas)
+        }
+    }
+
+    async requestAtiveBoardChanges(deltas: BoardDelta[]) {
+        this.requestBoardChanges(get(this.activeBoardHash), deltas)
+    }
+
+    getReadableBoardState(hash: EntryHashB64 | undefined) : Readable<BoardState> | undefined {
+        if (hash == undefined) return undefined
+        return this.boards[hash].workspace.state
+    }
+    
+    async getBoard(hash: EntryHashB64) : Promise<Board | undefined> {
+        let board = this.boards[hash]
+        if (!board) {
+            const workspaceHash = deserializeHash(hash)
+            board = this.boards[hash] = new Board(await this.boardsRootStore.joinWorkspace(workspaceHash));
+        }
+        return board
+    }
+
+    async setActiveBoard(hash: EntryHashB64 | undefined) {
+        let board
+        if (hash) {
+            board = await this.getBoard(hash)
+            if (board) {
+                this.activeBoardHash.update((n) => {return hash} )
+            }
+        }
+        if (!board) {
+            this.activeBoardHash.update((n) => {return undefined} )
+        }
+    }
+
+    async archiveBoard(hash: EntryHashB64) {
+        this.requestChanges([{type:"set-status", hash ,status:"archived"}])
+        // leave board and delete
+        const board: Board = this.boards[hash]
+        if (board) {
+            board.workspace.leaveWorkspace()
+            delete this.boards[hash]
+        }
+        if (get(this.activeBoardHash) == hash) {
+            this.setActiveBoard(undefined)
+        }
+    }
+
+    async unarchiveBoard(hash: EntryHashB64) {
+        let changes : BoardListDelta[] = 
+        [
+            {type:"set-status", hash ,status:""}
+        ]
+
+        this.requestChanges(changes)
+    }
+
+    closeActiveBoard() {
+        this.setActiveBoard(undefined)
+    }
+
+    async makeBoard(options: any, fromHash?: EntryHashB64) : Promise<Board> {
+        const board = await Board.Create(this.boardsRootStore)
+        const workspaceStore = board.workspace
+        const boardHash = board.hashB64()
+        console.log("NEW BOARD: ", board.hash(), workspaceStore.workspaceHash, boardHash)
+        this.boards[boardHash] = board 
+
+        if (options !== undefined) {
+            let changes = []
+            if (options.name) {
+                changes.push({
+                    type: "set-name",
+                    name: options.name
+                })
+            }
+            if (options.groups) {
+                changes.push({
+                    type: "set-groups",
+                    groups: options.groups
+                })
+            }
+            if (options.stickies) {
+                options.stickies.forEach((sticky)=>{
+                    changes.push({
+                        type: "add-sticky",
+                        value: sticky
+                    })
+                        
+                })
+            }
+            if (options.voteTypes) {
+                changes.push({
+                    type: "set-vote-types",
+                    voteTypes: options.voteTypes
+                })
+            }
+            if (changes.length > 0) {
+                workspaceStore.requestChanges(changes)
+                await workspaceStore.commitChanges()
+            }
+
+            this.requestChanges([{
+                type: 'add-board',
+                name: board.state().name,
+                hash: boardHash,
+                status: ""
+            }])
+        
+        }
+        return board
+    }
+}
