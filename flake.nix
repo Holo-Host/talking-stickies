dd664bb32ae9098248cbfc95308d043d3d584f08
Eric Harris-Braun <eric@harris-braun.com>
2023-06-12 13:57:16 -0400

    update flake

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
     holochain-flake = {
       url = "github:holochain/holochain";
       inputs.versions.url = "github:holochain/holochain/?dir=versions/0_1";
-      inputs.holochain.url = "github:holochain/holochain/holochain-0.1.5-beta-rc.0";
+      inputs.holochain.url = "github:holochain/holochain/holochain-0.1.5";
     };
   };
 
