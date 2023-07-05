f05d41f23c281e80c4e77bfa8bd38a45bf621074
Eric Harris-Braun <eric@harris-braun.com>
2023-07-05 09:25:43 -0400

    remove unneeded package and bump version

--- apps/launcher-kando/ui/src/main.ts
+++ apps/launcher-kando/ui/src/main.ts
@@ -1,6 +1,5 @@
 // @ts-ignore
 import App from './App.svelte'
-import "@webcomponents/scoped-custom-element-registry"
 const app = new App({
 	target: document.body,
 	props: {
--- apps/launcher/ui/src/main.ts
+++ apps/launcher/ui/src/main.ts
@@ -1,5 +1,4 @@
 import App from './App.svelte'
-import "@webcomponents/scoped-custom-element-registry"
 const app = new App({
 	target: document.body,
 	props: {
